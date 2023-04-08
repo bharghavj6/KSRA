ad1bf0cf89ae7e28ef9af85f34844132279449ee
Bhargav J <bharghavj6@gmail.com>
2023-04-08 22:40:00 +0530

    scroll

--- js/jscript.js
+++ js/jscript.js
@@ -4,16 +4,11 @@ jQuery(window).scroll(function(){
     if (scroll > 5) sticky.addClass('body-ns');
     else sticky.removeClass('body-ns');
   });
-  
- function openservice1(){
-window.location.href='service1.html'
-document.getElementById('service2').style.display="none";
-  }
 
   // For adding padding to carousal section related to header height
        $(document).ready(function ($) {
            // When the window resizes
-           $(window).on('resize', function () {
+           $(window).on('resize scroll', function () {
                // Get the height + padding + border of `#masthead`
              var mastHeight = $('.header-nav').outerHeight();
 
